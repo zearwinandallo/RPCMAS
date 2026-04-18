9f178ef89ff080a99cf33294ce4831c536fd2087
zearwinandallo <125346260+zearwinandallo@users.noreply.github.com>
2026-04-18 09:33:11 +0800

    Fix Price Update on Applied

--- RPCMAS.Infrastructure/Services/PriceChangeRequestService.cs
+++ RPCMAS.Infrastructure/Services/PriceChangeRequestService.cs
@@ -180,6 +180,8 @@ namespace RPCMAS.Infrastructure.Services
                 {
                     throw new Exception($"Item with SKU '{detail.SKU}' not found.");
                 }
+
+                item.CurrentPrice = detail.ProposedNewPrice;
             }
 
             request.Status = RequestStatusEnum.Applied;
